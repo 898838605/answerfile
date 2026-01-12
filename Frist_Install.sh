9ad92c0e72b5f43ed49ce0e1ae5a696a9ec33eae
898838605 <61263761+898838605@users.noreply.github.com>
2026-01-12 21:19:35 +0800

    Update Frist_Install.sh

--- Frist_Install.sh
+++ Frist_Install.sh
@@ -3,7 +3,8 @@ qemu-system-x86_64 -machine q35 -m 2048 -smp cpus=2 -cpu qemu64 \
 -netdev user,id=n1,hostfwd=tcp::2222-:22 \
 -device virtio-net,netdev=n1 \
 -cdrom alpine-virt-3.12.3-x86_64.iso \
--nographic alpine.img
+-drive file=alpine.img,if=virtio
+-nographic
 #qemu-system-x86_64启动虚拟机，参数为：
 #-machine q35：设定机型
 #-m 2048：分配 2 GB 内存
@@ -13,4 +14,5 @@ qemu-system-x86_64 -machine q35 -m 2048 -smp cpus=2 -cpu qemu64 \
 #-netdev user,id=n1,hostfwd=tcp::2222-:22：将 QEMU 的 22 端口映射到主机的 2222 端口，方便 SSH 连接
 #-device virtio-net,netdev=n1：启用 virtio 网络设备
 #-cdrom alpine-virt-3.12.3-x86_64.iso：挂载 Alpine 安装 ISO 文件
-#-nographic alpine.img：无图形界面，使用终端输入
+#-drive file=alpine.img,if=virtio：把本地文件 alpine.img 作为一块 virtio 硬盘挂给虚拟机，用来安装和运行 Alpine。
+#-nographic 无图形界面，使用终端输入
