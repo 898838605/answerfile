516002aa20a21e183682a96307b118037c333b0b
898838605 <61263761+898838605@users.noreply.github.com>
2026-01-12 20:00:16 +0800

    Create Frist_Install.sh for QEMU VM setup
    Add initial QEMU virtual machine setup script

---
+++ Frist_Install.sh
@@ -0,0 +1,16 @@
+qemu-system-x86_64 -machine q35 -m 2048 -smp cpus=2 -cpu qemu64 \
+-drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
+-netdev user,id=n1,hostfwd=tcp::2222-:22 \
+-device virtio-net,netdev=n1 \
+-cdrom alpine-virt-3.12.3-x86_64.iso \
+-nographic alpine.img
+#qemu-system-x86_64启动虚拟机，参数为：
+#-machine q35：设定机型
+#-m 2048：分配 2 GB 内存
+#-smp cpus=2：设定 2 个 CPU 核心
+#-cpu qemu64：使用 qemu64 CPU 模拟
+#-drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-x86_64-code.fd ：加载 UEFI 固件
+#-netdev user,id=n1,hostfwd=tcp::2222-:22：将 QEMU 的 22 端口映射到主机的 2222 端口，方便 SSH 连接
+#-device virtio-net,netdev=n1：启用 virtio 网络设备
+#-cdrom alpine-virt-3.12.3-x86_64.iso：挂载 Alpine 安装 ISO 文件
+#-nographic alpine.img：无图形界面，使用终端输入
